81655d8f3ccf346a516a4b7d7e690b3c7ca877d0
Janum Trivedi <jtrivedi@me.com>
2023-06-15 11:08:16 -0700

    Initial Commit

---
+++ MagnificationLoupe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MagnificationLoupe
+//
+//  Created by Janum Trivedi on 6/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MagnificationLoupe/MagnificationLoupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnificationLoupeApp.swift
+//  MagnificationLoupe
+//
+//  Created by Janum Trivedi on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MagnificationLoupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
